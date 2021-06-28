523f8351e14dd1cb5a7da8d33f40810658b2b44b
ianfleming464 <60020259+ianfleming464@users.noreply.github.com>
2021-06-28 06:45:39 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import PropTypes from "prop-types";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const FileList = ({ files }) => (
+	<table className='file-list'>
+		<tbody>
+			{files.map((file) => (
+				<tr className='file-list-item' key={file.id}>
+					<td className='file-name'>{file.name}</td>{" "}
+				</tr>
+			))}
+		</tbody>
+	</table>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const testFiles = [
+	{
+		id: 1,
+		name: "src",
+		type: "folder",
+		updated_at: "2016-07-11 21:24:00",
+		latestCommit: {
+			message: "Initial commit",
+		},
+	},
+	{
+		id: 2,
+		name: "tests",
+		type: "folder",
+		updated_at: "2016-07-11 21:24:00",
+		latestCommit: {
+			message: "Initial commit",
+		},
+	},
+	{
+		id: 3,
+		name: "README",
+		type: "file",
+		updated_at: "2016-07-18 21:24:00",
+		latestCommit: {
+			message: "Added a readme",
+		},
+	},
+];
+// put the ReactDOM.render call here
+// pass testFiles as FileList's file prop
+ReactDOM.render(<FileList files={testFiles} />, document.querySelector("#root"));
+
+FileList.propTypes = {
+	files: PropTypes.array,
+};
